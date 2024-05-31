36c4ddf2d89268a961b8a9fbc552af4b52246ebb
Richard Burman <richard.burman@harperjames.co.uk>
2024-05-31 15:55:25 +0100

    Sending a delete Request and connecting to clear posts button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -12,51 +13,37 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
-  }
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+  });
+}
 
   onCreatePost(postData: Post ) {
     // Send Http request
-    this.http
-    .post<{name: string}>(
-      'https://angular-http-project-8f437-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
-     postData
-    ).subscribe(responseData => {
-      console.log(responseData);
-    });
+   this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts();
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http
-    .get<{ [key: string]: Post }>('https://angular-http-project-8f437-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
-    .pipe(
-      map(responseData => {
-      const postsArray: Post[] = [];
-      for (const key in responseData) {
-        if (responseData.hasOwnProperty(key)){
-          postsArray.push({ ...responseData[key], id: key })
-        }
-      }
-      return postsArray;
-    }))
-    .subscribe(posts => {
-      // ...
-      this.isFetching = false;
-      this.loadedPosts = posts;
-      
+    this.postsService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
     });
   }
+
 }
+
+
